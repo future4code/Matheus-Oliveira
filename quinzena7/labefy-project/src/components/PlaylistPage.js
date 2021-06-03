08826e6b97d1fa3c64264201df9ff43d980a651d
Matheus <matheusrdeoliv1@gmail.com>
2021-06-02 23:44:12 -0300

    agora as musicas tocam no proprio site

--- quinzena7/labefy-project/src/components/PlaylistPage.js
+++ quinzena7/labefy-project/src/components/PlaylistPage.js
@@ -1,56 +1,74 @@
 import React from 'react'
 import axios from 'axios'
+import ReactPlayer from 'react-player'
 
 export default class PlaylistPage extends React.Component {
-    state = {
-      inputMusicName: "",
-      inputArtist: "",
-      inputUrl: "",
-      playlistTracks: []
-    }
+  state = {
+    inputMusicName: "",
+    inputArtist: "",
+    inputUrl: "",
+    playlistTracks: []
+  }
   
-    onChangeInputMusic = (event) => {
-      this.setState({inputMusicName: event.target.value})
-    }
+  onChangeInputMusic = (event) => {
+    this.setState({inputMusicName: event.target.value})
+  }
   
-    onChangeInputArtist = (event) => {
-      this.setState({inputArtist: event.target.value})
-    }
+  onChangeInputArtist = (event) => {
+    this.setState({inputArtist: event.target.value})
+  }
   
-    onChangeInputUrl = (event) => {
-      this.setState({inputUrl: event.target.value})
-    }
+  onChangeInputUrl = (event) => {
+    this.setState({inputUrl: event.target.value})
+  }
 
-    addTrackPlaylist = () => {
-      const body = {
-        name: this.state.inputMusicName, 
-        artist: this.state.inputArtist,
-        url: this.state.inputUrl
-    }
-      axios
-        .post(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${this.props.selectedPlaylistId}/tracks`, body, {
-          headers: {
-            Authorization: 'matheus-rodrigues-munoz'
-          }
-        }).then(() => {
-          alert("Track successfully added to your playlist")
-          {this.getAllTracks()}
-        }).catch(() => {
-          alert("No music, artist or url found. Try again")
+  addTrackPlaylist = () => {
+    const body = {
+      name: this.state.inputMusicName, 
+      artist: this.state.inputArtist,
+      url: this.state.inputUrl
+  }
+    axios
+      .post(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${this.props.selectedPlaylistId}/tracks`, body, {
+        headers: {
+          Authorization: 'matheus-rodrigues-munoz'
+        }
+      }).then(() => {
+        alert("Track successfully added to your playlist")
+        {this.getAllTracks()}
+      }).catch(() => {
+        alert("No music, artist or url found. Try again")
+      })
+  }
+
+  getAllTracks = () => {
+    axios
+      .get(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${this.props.selectedPlaylistId}/tracks`, {
+        headers: {
+          Authorization: 'matheus-rodrigues-munoz'
+        }
+        }).then((res) => {
+          this.setState({ playlistTracks: res.data.result.tracks })
+        }).catch((err) => {
+          alert("Error to get your playlist tracks", err.data)
         })
-      }
+  }
 
-    getAllTracks = () => {
-      axios
-        .get(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${this.props.selectedPlaylistId}/tracks`, {
-          headers: {
-            Authorization: 'matheus-rodrigues-munoz'
-          }
-          }).then((res) => {
-            this.setState({ playlistTracks: res.data.result.tracks })
-          }).catch((err) => {
-            alert("Error to get your playlist tracks", err.data)
+  deletePlaylist = (trackDeletedId) => {
+    const deleteTrack = trackDeletedId
+      if( window.confirm("Do you really want to delete this track?")) {
+        axios
+          .delete(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${this.props.selectedPlaylistId}/tracks/${deleteTrack}`, {
+            headers: { 
+              Authorization: 'matheus-rodrigues-munoz'
+            }
+          }).then(() => {
+              alert('Track deleted!')
+              {this.getAllTracks()}
+          }).catch((err) =>{
+            alert('Error.', err.data)
           })
+      }
     }
 
     componentDidMount() {
@@ -61,8 +79,9 @@ export default class PlaylistPage extends React.Component {
       const mapTracks = this.state.playlistTracks.map((tracks) => {
         return (
           <div key={tracks.id}>
-            <a href={tracks.url}>{tracks.name}</a>
-            <button>x</button>
+            <h2>{tracks.name}</h2>
+            <ReactPlayer url={tracks.url} controls/>
+            <button onClick={() => this.deletePlaylist(tracks.id)}>x</button>
           </div>
       )
     })
