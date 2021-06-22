724562f2030e2ef68c86deecbabdde797b163c03
Matheus <matheusrdeoliv1@gmail.com>
2021-06-22 17:15:45 -0300

    criando layout do fluxo publico de paginas

---
+++ quinzena9/labex/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import HomePage from "./pages/HomePage"
+import ListTripPage from "./pages/ListTripsPage"
+import AplicationFromPage from "./pages/ApplicationFormPage"
+
+export default function App() {
+  return (
+    <>
+      <HomePage />
+      <ListTripPage />
+      <AplicationFromPage />
+    </>
+  )
+}
